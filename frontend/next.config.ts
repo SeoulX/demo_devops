fd3532aaf161f86b886b6d39fc5fc0e221554bbe
Andrian <mabinas@tip.edu.ph>
2025-03-18 13:12:05 +0800

    Fix the connection of backend and frontend part 20

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   env: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ||  'http://54.151.219.161:8000'
   },
 };
 
