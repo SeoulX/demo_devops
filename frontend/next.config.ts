12826a06f40c1cb6d535f4e48d613074ee5004d3
Andrian <mabinas@tip.edu.ph>
2025-03-21 03:54:12 +0800

    theory 2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  env: {
+      NEXT_PUBLIC_API_URL: 'http://fastapi-nextjs-connection-875703309.ap-southeast-1.elb.amazonaws.com/get_init',
+  },
 };
 
 export default nextConfig;
